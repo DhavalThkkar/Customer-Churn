01eac500ab4ed77f6d6dfdc1c9ddd18b3fb4ad37
Unknown <thakkar_@outlook.com>
2017-05-07 23:46:23 +0530

    Cleaned and organized with some problems
    There are issues in one hot encoding of Int'l  Plans and VMail Plans

--- Without Null values(Dropped).py
+++ Without Null values(Dropped).py
@@ -11,26 +11,44 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('churn.csv')
-df.drop(labels = 'Unnamed: 0',axis = 1, inplace = True)
-df.drop(labels = 'Phone',axis = 1, inplace = True)
-df_NaN = df.replace('?',np.nan,inplace = True)
+# Importing the Dataset
+data = pd.read_csv('churn.csv')
+
+# Replacing all the '?' with np.nan
+data_NaN = data.replace('?',np.nan,inplace = True)
+
+# Setting the dependent Variabke 'Churn?' to 0's and 1's from False and True
 def boolToInt(a):
     if a == 'True.':
         return 1
     elif a == 'False.':
         return 0
-df['Churn?'] = df['Churn?'].apply(lambda x: boolToInt(x))
+data['Churn?'] = data['Churn?'].apply(lambda x: boolToInt(x))
 
+#Removing 'Unnamed: 0' and 'Phone' columns
+data.drop(labels = 'Unnamed: 0',axis = 1, inplace = True)
+data.drop(labels = 'Phone',axis = 1, inplace = True)
 
-# Encoding categorical data
+# Count of NaN values in the Dataset
+print(data.isnull().sum())
+
+# Visulalizing the dataframe
+plt.figure(figsize=(13,10))
+sns.heatmap(data = data.corr(), cmap= 'viridis')
+
+# Encoding categorical data of the Dataframe
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X_1 = LabelEncoder()
-df['State'] = labelencoder_X_1.fit_transform(df['State'])
+data['State'] = labelencoder_X_1.fit_transform(data['State'])
 labelencoder_X_2 = LabelEncoder()
-df['VMail Plan'] = labelencoder_X_2.fit_transform(df['VMail Plan'])
+data['VMail Plan'] = labelencoder_X_2.fit_transform(data['VMail Plan'])
 
 
+
+
+
+
+# To be done...
 X = df.drop(labels = 'Churn?', axis=1)
 y = df.iloc[:, 20].values
 
@@ -42,11 +60,8 @@ onehotencoder = OneHotEncoder(categorical_features = [1])
 df = onehotencoder.fit_transform(df)
 X = X[:, 1:]
 
-# Visualizing
 
-print(df.isnull().sum())
-plt.figure(figsize=(13,10))
-sns.heatmap(data = df.corr(), cmap= 'viridis')
+
 
 # Splitting into TRAIN, TEST set
 from sklearn.model_selection import train_test_split
