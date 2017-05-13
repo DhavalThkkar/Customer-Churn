ab30dc628c7a5a3de110e754d7c6bbea1e3d31df
DhavalThkkar <thakkar_@outlook.com>
2017-05-13 08:14:53 +0530

    Tuning Error need to be solved

--- CustomerChurn.py
+++ CustomerChurn.py
@@ -12,6 +12,7 @@ import matplotlib.pyplot as plt
 
 # Sklearn
 from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import roc_curve,auc,roc_auc_score
 
 # Keras
 from keras.models import Sequential
@@ -53,14 +54,54 @@ data.drop('State', axis=1, inplace=True)
 X = pd.concat([data, state], axis=1).as_matrix()
 
 kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=1)    
-
 for train, test in kfold.split(X, y):
+    
+    """
+    from sklearn.preprocessing import StandardScaler
+    sc = StandardScaler()
+    X[train] = sc.fit_transform(X[train])
+    X[test] = sc.transform(X[test])"""
     # Initial tests appear to indicate no overfitting, dropout layer unneccesary
     ann = Sequential()    
-    ann.add(Dense(30, activation='tanh', kernel_initializer='random_normal', input_shape=(X[train].shape[1],)))
+    ann.add(Dense(100, activation='tanh', kernel_initializer='random_normal', input_shape=(X[train].shape[1],)))
+    ann.add(Dropout(rate=0.1))
+    ann.add(Dense(100, activation='tanh', kernel_initializer='random_normal'))
+    ann.add(Dropout(rate=0.1))
     ann.add(Dense(1, activation='sigmoid', kernel_initializer='random_normal'))
     ann.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
     
     ann.fit(X[train], y[train], batch_size=100, epochs=150, verbose=0)
+    fpr, tpr, _ = roc_curve(y[test],ann.predict_proba(X[test]))
+    auc_score = auc(fpr,tpr)
     
+    print('AUC Score: {:.3f}%'.format(auc_score))
     print('State Dummy Variables Accuracy: {:.2f}%'.format(ann.evaluate(X[test], y[test], verbose=0)[1]*100))
+
+# Tuning the ANN
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import GridSearchCV
+from keras.models import Sequential
+from keras.layers import Dense
+def build_classifier(optimizer):
+    classifier = Sequential()
+    classifier.add(Dense(units = units, kernel_initializer = 'random_normal', activation = 'tanh', input_shape = (X_train.shape[1],)))
+    classifier.add(Dropout(rate = 0.1))
+    classifier.add(Dense(units = units, kernel_initializer = 'random_normal', activation = 'tanh'))
+    classifier.add(Dropout(rate = 0.1))
+    classifier.add(Dense(units = 1, kernel_initializer = 'random_normal', activation = 'sigmoid'))
+    classifier.compile(optimizer = optimizer, loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return classifier
+classifier = KerasClassifier(build_fn = build_classifier)
+parameters = {'batch_size': [50,100,150,200,250,300],
+              'units' : [50,100,150],
+              'epochs': [100, 500],
+              'optimizer': ['adam']}
+grid_search = GridSearchCV(estimator = classifier,
+                           param_grid = parameters,
+                           scoring = 'accuracy',
+                           cv = 10)
+grid_search = grid_search.fit(X_train, y_train)
+best_parameters = grid_search.best_params_
+best_accuracy = grid_search.best_score_
